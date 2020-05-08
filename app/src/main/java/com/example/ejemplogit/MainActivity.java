37126bb2e68105a8fc21691342fe08c8762ccedd
raul <raulblazquezhernangomez@gmail.com>
2020-05-08 18:35:48 +0200

    ejemplo

---
+++ app/src/main/java/com/example/ejemplogit/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ejemplogit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int primer = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
